170e5fa6b53a17c07c7df1bb6dad4002224e87e1
itishprasad30 <itishprasad20@gmail.com>
2022-05-15 08:07:16 +0530

    some changes are made

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const playPauseBtn = document.querySelector(".play-pause-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
+const miniPlayerBtn = document.querySelector(".mini-play-btn");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
-console.log(videoContainer);
+document.addEventListener("keydown", (e) => {
+  switch (e.key.toLowerCase()) {
+    case " ":
+    case "k":
+      togglePlay();
+      break;
+  }
+});
+
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toogleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toogleMiniPlayerMode);
+
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+function toogleFullScreenMode() {
+  videoContainer.classList.toggle("full-screen");
+}
+function toogleMiniPlayerMode() {}
+
+//  Play pause funtionality
 playPauseBtn.addEventListener("click", togglePlay);
+
+video.addEventListener("click", togglePlay);
 
 function togglePlay() {
   video.paused ? video.play() : video.pause();
