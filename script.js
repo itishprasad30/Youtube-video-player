10a3d0bcb4bb4b5bec3b140f1dabf9254789dbae
itishprasad30 <itishprasad20@gmail.com>
2022-05-15 12:35:13 +0530

    minor changes added

--- script.js
+++ script.js
@@ -2,17 +2,65 @@ const playPauseBtn = document.querySelector(".play-pause-btn");
 const theaterBtn = document.querySelector(".theater-btn");
 const fullScreenBtn = document.querySelector(".full-screen-btn");
 const miniPlayerBtn = document.querySelector(".mini-play-btn");
+const muteBtn = document.querySelector(".mute-btn");
+const volumeSlider = document.querySelector(".volume-slider");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
 document.addEventListener("keydown", (e) => {
+  const tagName = document.activeElement.tagName.toLowerCase();
+
+  if (tagName === "input") return;
+
   switch (e.key.toLowerCase()) {
     case " ":
+      if (tagName === "button") return;
     case "k":
       togglePlay();
       break;
+
+    case "f":
+      toogleFullScreenMode();
+      break;
+    case "i":
+      toogleMiniPlayerMode();
+      break;
+
+    case "t":
+      toggleTheaterMode();
+      break;
   }
 });
+//volume controls
+
+muteBtn.addEventListener("click", toggleVolume);
+volumeSlider.addEventListener("input", (e) => {
+  video.volume = e.target.value;
+  video.muted = e.target.value === 0;
+});
+
+function toggleVolume() {
+  video.muted = !video.muted;
+}
+
+video.addEventListener("volumechange", () => {
+  volumeSlider.value = video.volume;
+  let volumeLevel;
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0;
+    volumeLevel = "muted";
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high";
+  } else {
+    volumeLevel = "low";
+  }
+
+  videoContainer.dataset.volumeLevel = volumeLevel;
+
+  video.muted, video.volume;
+});
+
+// different modes
 
 theaterBtn.addEventListener("click", toggleTheaterMode);
 fullScreenBtn.addEventListener("click", toogleFullScreenMode);
@@ -22,9 +70,31 @@ function toggleTheaterMode() {
   videoContainer.classList.toggle("theater");
 }
 function toogleFullScreenMode() {
-  videoContainer.classList.toggle("full-screen");
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
 }
-function toogleMiniPlayerMode() {}
+function toogleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player");
+});
+
+video.addEventListener("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player");
+});
 
 //  Play pause funtionality
 playPauseBtn.addEventListener("click", togglePlay);
