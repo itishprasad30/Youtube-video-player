d1fd630108f8dc453649571508b601e7c651f62d
itishprasad30 <itishprasad20@gmail.com>
2022-05-16 19:51:09 +0530

    preview img not woring

--- script.js
+++ script.js
@@ -59,16 +59,49 @@ document.addEventListener("keydown", (e) => {
 // Timeline
 
 timelineContainer.addEventListener("mousemove", handleTimelineUpdate);
+timelineContainer.addEventListener("mousedown", toggleScrubbing);
+document.addEventListener("mouseup", (e) => {
+  if (isScrubbing) toggleScrubbing(e);
+});
+document.addEventListener("mousemove", (e) => {
+  if (isScrubbing) handleTimelineUpdate(e);
+});
+
+let isScrubbing = false;
+let wasPaused;
+function toggleScrubbing(e) {
+  const rect = timelineContainer.getBoundingClientRect();
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  isScrubbing = (e.buttons & 1) === 1;
+  videoContainer.classList.toggle("scrubbing", isScrubbing);
+  if (isScrubbing) {
+    wasPaused = video.paused;
+    video.pause();
+  } else {
+    video.currentTime = percent * video.duration;
+    if (!wasPaused) video.play();
+  }
+
+  handleTimelineUpdate(e);
+}
+
 function handleTimelineUpdate(e) {
   const rect = timelineContainer.getBoundingClientRect();
   const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
-
   const previewImgNumber = Math.max(
     1,
-    Math.floor(percent * videoDuration) / 10
+    Math.floor((percent * video.duration) / 10)
   );
-}
+  // const previewImgSrc = `assets/previewImgs/preview${previewImgNumber}.jpg`;
+  // previewImg.src = previewImgSrc;
+  timelineContainer.style.setProperty("--preview-position", percent);
 
+  if (isScrubbing) {
+    e.preventDefault();
+    // thumbnailImg.src = previewImgSrc;
+    timelineContainer.style.setProperty("--progress-position", percent);
+  }
+}
 // playback speed
 
 speedBtn.addEventListener("click", changePlaybackSpeed);
